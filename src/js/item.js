b1ce956e1d6e1aac4d9fe605b7a400eecff12216
Mihazzz123 <m.dokuchaev73@gmail.com>
2019-07-29 23:11:02 +0300

    делаю  адаптив для страницы с товаром

--- src/js/item.js
+++ src/js/item.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
 
 
-  var btnAddInfo = $('.item-text__btn'),
-      resultAddInfo = $('.item_resBox'),
-      btnAddInfo_icon = $('.item-text__btnIcon');
+  var btnAddInfo = $('.item-text__btnD'),
+      resultAddInfo = $('.item_resBoxD'),
+      btnAddInfo_icon = $('.item-text__btnIconD');
    
-  $('.item-text__btns').on('click', '.item-text__btn', function() {
-    var activ = $(this).find( btnAddInfo_icon ).attr('data-src-second');
-    $(this).find( btnAddInfo_icon) .attr('src', activ);
+  $('.item-text__btnsD').on('click', '.item-text__btnD', function() {
     var i = btnAddInfo.index($(this));
+    var active = btnAddInfo_icon.eq(i).attr('data-src-second');
+    var pasive = btnAddInfo_icon.eq(0).attr('data-src');
+    var pasive1 = btnAddInfo_icon.eq(1).attr('data-src');
+    var pasive2 = btnAddInfo_icon.eq(2).attr('data-src');
+    var pasive3 = btnAddInfo_icon.eq(3).attr('data-src');
     resultAddInfo.removeClass('dflex');
     resultAddInfo.eq(i).addClass('dflex');
     btnAddInfo.removeClass('item-text__btn_active');
     btnAddInfo.eq(i).addClass('item-text__btn_active');
+    btnAddInfo_icon.eq(0).attr('src', pasive);
+    btnAddInfo_icon.eq(1).attr('src', pasive1);
+    btnAddInfo_icon.eq(2).attr('src', pasive2);
+    btnAddInfo_icon.eq(3).attr('src', pasive3);
+    if(btnAddInfo.hasClass('item-text__btn_active')){
+      btnAddInfo_icon.eq(i).attr('src', active);
+    } 
   });
 
   $('.item-buttons__nav').on('click', '.item-buttons__btn', function() {
@@ -109,6 +119,7 @@ $(document).ready(function() {
 
   // Слайдер 
   var slider = $('#slider'),
+      sliderM = $('#m_slider'),
       imgActive = $('#imgActive'),
       imgPasive = $('.imgPasive'),
       imgBar = $('.item-slider__slideBar'),
@@ -120,7 +131,11 @@ $(document).ready(function() {
       slider.on('click', 'a', function(event) {
         event.preventDefault();
       });
+      sliderM.on('click', 'a', function(event) {
+        event.preventDefault();
+      });
       
+      // Смена активной картинки
       imgBar.on('click', '.imgPasive', function() {
         var i = imgPasive.index($(this)),
         srcImgPas = imgPasive.eq(i).attr('data-src');
@@ -137,22 +152,75 @@ $(document).ready(function() {
         imgBtn.eq(i).css({'border' : '1px solid #ccc'});
       });
       
-
+      
+      // Стрелки навигации
       btnLeft.click(function() {
         var valBtn = imgBtn.attr('data-src-val');
-        var  b = 82,
-        c = +valBtn + +b;
-        imgBtn.css({'transform' : 'translate3d(' + c + 'px, 0px, 0px)'});
-        imgBtn.attr('data-src-val', c);
-        console.log(valBtn);
+        var  b = 100;
+        var c = +valBtn + +b;
+        if(valBtn == 0){
+          btnRight.css({'opacity' : '1'});
+          btnLeft.css({'opacity' : '0.3'});
+        } else {
+          btnRight.css({'opacity' : '1'});
+          imgBtn.css({'transform' : 'translate3d(' + c + '%, 0px, 0px)'});
+          imgBtn.attr('data-src-val', c);
+        } console.log(c);
       });
+      
+      
       btnRight.click(function() {
         var valBtn = imgBtn.attr('data-src-val');
-        var  b = 82,
-        c = +valBtn - +b;
-        imgBtn.css({'transform' : 'translate3d(' + c + 'px, 0px, 0px)'});
-        imgBtn.attr('data-src-val', c);
+        var  b = 100;
+        var c = +valBtn - +b;
+        if (valBtn == -200){
+          btnLeft.css({'opacity' : '1'});
+          btnRight.css({'opacity' : '0.3'});
+        } else {
+          imgBtn.css({'transform' : 'translate3d(' + c + '%, 0px, 0px)'});
+          imgBtn.attr('data-src-val', c);
+          btnLeft.css({'opacity' : '1'});
+        }
         console.log(valBtn);
       });
       
-});
+      if($(window).width() > '1200'){
+        var itemSlide = imgBtn.length;
+        if(itemSlide == 5){
+          btnLeft.hide();
+          btnRight.hide();
+        }
+      };
+      console.log(itemSlide);
+
+
+
+
+      // MOBILE
+
+  $('.item-text__button').on('click', function() {
+    var active = $(this).find('.item-text__btnIconM').attr('data-src-second');
+    var pasive = $(this).find('.item-text__btnIconM').attr('data-src');
+   
+    if($(this).find('.item-text__button').hasClass('.tem-text__button_active')){
+      $(this).find('.item-text__btnIconM').attr('src', pasive);
+      $(this).find('.item-text__btnClose').removeClass('item-text__btnClose_active');
+      $(this).find('.item-text__btn').removeClass('item-text__btn_active');
+    }
+    
+    $('.item-text__button').removeClass('item-text__button_active');
+    
+    $(this).find('.item-text__btnIconM').attr('src', active);
+    $(this).toggleClass('item-text__button_active');
+    $(this).find('.item-text__btn').toggleClass('item-text__btn_active');
+    $(this).find('.item-text__btnClose').toggleClass('item-text__btnClose_active');
+    
+    if($(this).find('.item-text__button').hasClass('.tem-text__button_active')){
+      $(this).find('.item-text__btn').addClass('item-text__btn_active');
+      $(this).find('.item-text__btnIconM').attr('src', active);
+    }
+
+
+  });
+      
+    });
